098a87f14f316c64dd43ebb5dd472eb806f19404
sarath2003-sketch <sarathsanthons@gmail.com>
2026-08-09 14:04:32 +0530

    Fix Railway Linux MODULE_NOT_FOUND case-sensitivity crash for payments.js

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const membersRoutes = require('./server/routes/members');
 const memberAuthRoutes = require('./server/routes/member-auth');
 const memberPaymentsRoutes = require('./server/routes/member-payments');
 const adminPaymentsRoutes = require('./server/routes/admin-payments');
-const paymentsRoutes = require('./server/routes/payments');
+let paymentsRoutes;
+try {
+  paymentsRoutes = require('./server/routes/payments');
+} catch (e) {
+  paymentsRoutes = require('./server/routes/Payments');
+}
 const withdrawalsRoutes = require('./server/routes/withdrawals');
 const transactionsRoutes = require('./server/routes/transactions');
 const dashboardRoutes = require('./server/routes/dashboard');
